090cd348a43c3e08672027b39e9e89f89a3d41b1
Jean-Stefan <jeanss12@gmail.com>
2022-06-12 16:08:58 -0300

    Style: Added styling to html, body, root element

--- src/globalStyle.jsx
+++ src/globalStyle.jsx
@@ -1,19 +1,26 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
 *{
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
 }
-	html{
-		height: 100%;
-	}
-	body{
-		min-height: 100%;
-		background: #ECEEF2;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+
+html{
+	height: 100%;
+}
+
+body{
+	background: #ECEEF2;
+	height: 100%;
+}
+
+#root{
+	min-height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 `;
