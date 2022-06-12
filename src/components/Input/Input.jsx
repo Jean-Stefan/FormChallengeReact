4f12ba87c2452f5b57b13ec83488c43002aa9762
Jean-Stefan <jeanss12@gmail.com>
2022-06-12 16:43:07 -0300

    Style: Added Input styling

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Input = styled.input``;
+export const Input = styled.input`
+    height: 40px;
+    padding: 12px;
+    border: 2px solid #aaaaaa;
+    border-radius: 4px;
+    font-size: 1rem;
+    flex: 1;
+`;
