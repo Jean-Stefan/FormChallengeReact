61750ec54b8596dbc2f91b7da55e99caab423f5b
Jean-Stefan <jeanss12@gmail.com>
2022-06-12 16:42:07 -0300

    Style: Added maxWidth, borderRadius, padding, flex

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
     display: flex;
+    flex: 1;
     background: #ffffff;
+    max-width: 620px;
+    border-radius: 20px;
+    padding: 40px 50px 60px 50px;
 `;
