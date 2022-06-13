b7f2ae3127ebc1f9e97feaab053cfaca10c81939
Jean-Stefan <jeanss12@gmail.com>
2022-06-13 18:48:23 -0300

    Feat: Added Title component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-family: 'Inter', Helvetica;
+    font-size: 2.25rem;
+    text-align: center;
+    margin-bottom: ${({mb}) => mb};
+`;
