64078871c623572879d1931f9aad4dc1a2278069
Jean-Stefan <jeanss12@gmail.com>
2022-06-13 18:38:23 -0300

    Feat: Created Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
+export const Label = styled.label`
+    font-size: 0.875rem;
+    color: #767676;
+`;
+
 export const Input = styled.input`
+    width: 100%;
     height: 40px;
     padding: 12px;
     border: 2px solid #aaaaaa;
