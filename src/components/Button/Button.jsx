6169199a52fc0388c7f93af0a66992c18d8b0a28
Jean-Stefan <jeanss12@gmail.com>
2022-06-14 13:05:06 -0300

    Feat: Added Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    color: #ffffff;
+    background: #0dbdbd;
+    border-radius: 10px;
+    border-color: transparent;
+    padding: 11px;
+    :hover {
+        filter: brightness(0.9);
+    }
+`;
