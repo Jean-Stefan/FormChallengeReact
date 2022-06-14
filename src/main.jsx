121641b712aa40b2241d5077f092bd8e1bf743b0
Jean-Stefan <jeanss12@gmail.com>
2022-06-14 17:41:37 -0300

    Feat: Created routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import {Success} from './pages/Success/Success';
 import {GlobalStyle} from './globalStyle';
+import {Home} from './pages/Home/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GlobalStyle />
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='success' element={<Success />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
 );
