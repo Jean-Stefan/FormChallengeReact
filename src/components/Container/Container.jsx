a82f52d675beeacafea0dd38b286336e1ff65f1d
Jean-Stefan <jeanss12@gmail.com>
2022-06-14 18:22:10 -0300

    Feat: Created Success page

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -6,11 +6,13 @@ export const Container = styled.form`
     flex: 1;
     background: #ffffff;
     max-width: 620px;
+    height: 685px;
     border-radius: 20px;
     padding: 40px 50px 60px 50px;
 
     @media (max-width: 768px) {
         padding: 0 20px;
         border-radius: 0;
+        height: 100%;
     }
 `;
