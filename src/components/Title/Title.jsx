365717b784b616dd89dccbdecc1f9ff72d72883f
Jean-Stefan <jeanss12@gmail.com>
2022-06-14 14:16:11 -0300

    Style: Added responsive style for Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -5,4 +5,8 @@ export const Title = styled.h1`
     font-size: 2.25rem;
     text-align: center;
     margin-bottom: ${({mb}) => mb};
+
+    @media (max-width: 768px) {
+        margin-bottom: 15px;
+    }
 `;
