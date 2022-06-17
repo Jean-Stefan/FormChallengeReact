dbd1cda860d5d037d92c8274abeed53a284a2bde
Jean-Stefan <jeanss12@gmail.com>
2022-06-17 15:47:00 -0300

    Feat: Added file yupSchema with input validations

---
+++ src/utils/yupSchema.js
@@ -0,0 +1,31 @@
+import * as yup from 'yup';
+
+export const schema = yup
+    .object({
+        name: yup
+            .string()
+            .matches(/^[\w]+(?:\s[\w]+)+$/, 'Fullname invalid')
+            .required('Fullname invalid'),
+        email: yup
+            .string()
+            .matches(
+                /^[a-z0-9._-]+(?:\.[a-z0-9._-]+)*@(?:[a-z0-9](?:[a-z-]*[a-z])?.)+[a-z](?:[a-z]*[a-z]){1,}?$/,
+                'Email invalid',
+            )
+            .required('Email invalid'),
+        password: yup
+            .string()
+            .matches(/^[0-9]{6,9}$/, 'Password invalid')
+            .required('Password invalid'),
+        phone: yup
+            .string()
+            .matches(/^([(][0-9]{2}[)]) ([0-9]{5})-([0-9]{4})/, 'Phone invalid')
+            .required(),
+        birthday: yup
+            .date()
+            .typeError('Age invalid')
+            .min('1899-01-01', 'Age invalid')
+            .required('Age invalid'),
+        checkbox: yup.boolean().isTrue('You must agree with terms'),
+    })
+    .required();
