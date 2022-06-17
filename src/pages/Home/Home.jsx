6aed5de65f28f6c38c3c265aff3b69ad7b3c76f5
Jean-Stefan <jeanss12@gmail.com>
2022-06-17 15:44:58 -0300

    Refactor: moved schema to another file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,23 @@
+import {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import styled from 'styled-components';
+
+import {schema} from '../../utils/yupSchema';
+import {phoneMask} from '../../utils/phoneMask';
+
+import {
+    Form,
+    FlexRow,
+    FlexColumn,
+    GroupBirthday,
+    GroupEmail,
+    GroupName,
+    GroupPassword,
+    GroupPhone,
+    Error,
+    CheckmarkError,
+} from './StylesHome';
 
 import {Container} from '../../components/Container/Container';
 import {Input, Label} from '../../components/Input/Input';
@@ -10,49 +25,44 @@ import {Image} from '../../components/Image/Image';
 import {Title} from '../../components/Title/Title';
 import {Button} from '../../components/Button/Button';
 import {Checkbox} from '../../components/Checkbox/Checkbox';
-import {phoneMask} from '../../utils/phoneMask';
-
-const schema = yup
-    .object({
-        name: yup
-            .string()
-            .matches(/^[\w]+(?:\s[\w]+)+$/, 'Fullname invalid')
-            .required('Fullname invalid'),
-        email: yup
-            .string()
-            .matches(
-                /^[a-z0-9._-]+(?:\.[a-z0-9._-]+)*@(?:[a-z0-9](?:[a-z-]*[a-z])?.)+[a-z](?:[a-z]*[a-z]){1,}?$/,
-                'Email invalid',
-            )
-            .required('Email invalid'),
-        password: yup
-            .string()
-            .matches(/^[0-9]{6,9}$/, 'Password invalid')
-            .required('Password invalid'),
-        phone: yup
-            .string()
-            .matches(/^([(][0-9]{2}[)]) ([0-9]{5})-([0-9]{4})/, 'Phone invalid')
-            .required(),
-        birthday: yup
-            .date()
-            .typeError('Age invalid')
-            .min('1899-01-01', 'Age invalid')
-            .required('Age invalid'),
-        checkbox: yup.boolean().isTrue('You must agree with terms'),
-    })
-    .required();
 
 export const Home = () => {
     const navigate = useNavigate();
+
     const {
         register,
+        getValues,
+        setValue,
         handleSubmit,
         formState: {errors},
     } = useForm({resolver: yupResolver(schema)});
 
     const onSubmit = (formData) => {
+        localStorage.setItem('data', JSON.stringify(formData));
         navigate('/success');
     };
+
+    const unload = () => {
+        localStorage.setItem('data', JSON.stringify(getValues()));
+    };
+
+    const getLocalStorage = () => {
+        if (localStorage.getItem('data')) {
+            const data = JSON.parse(localStorage.getItem('data'));
+            const keys = Object.keys(data);
+            keys.forEach((key) => {
+                setValue(key, data[key]);
+            });
+        }
+    };
+
+    useEffect(() => {
+        getLocalStorage();
+        window.addEventListener('beforeunload', unload);
+        return () => {
+            window.removeEventListener('beforeunload', unload);
+        };
+    }, []);
 
     return (
         <Container onSubmit={handleSubmit(onSubmit)}>
@@ -151,83 +161,3 @@ export const Home = () => {
         </Container>
     );
 };
-
-const Form = styled.form``;
-
-const FlexRow = styled.div`
-    display: flex;
-    justify-content: space-between;
-
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
-`;
-
-const FlexColumn = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex: ${({flex}) => flex};
-
-    @media (max-width: 768px) {
-        flex-direction: ${({responsive}) => responsive};
-    }
-`;
-
-const FormGroup = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    margin-bottom: 40px;
-
-    @media (max-width: 768px) {
-        margin-bottom: 15px;
-    }
-`;
-
-const GroupName = styled(FormGroup)``;
-
-const GroupEmail = styled(FormGroup)`
-    margin-right: 25px;
-
-    @media (max-width: 768px) {
-        margin-right: 0px;
-    }
-`;
-
-const GroupPassword = styled(FormGroup)`
-    margin-right: 25px;
-
-    @media (max-width: 768px) {
-        margin-right: 0px;
-    }
-`;
-
-const GroupPhone = styled(FormGroup)`
-    @media (max-width: 768px) {
-        width: 50%;
-        flex: 1;
-        margin-right: 25px;
-        margin-bottom: 25px;
-    }
-`;
-
-const GroupBirthday = styled(FormGroup)`
-    max-height: 69px;
-
-    @media (max-width: 768px) {
-        width: calc(50% - 15px);
-        flex: 1;
-        margin-bottom: 25px;
-    }
-`;
-
-const Error = styled.span`
-    color: red;
-    font-size: 14px;
-    position: absolute;
-    margin-top: 70px;
-`;
-
-const CheckmarkError = styled(Error)`
-    margin-top: 30px;
-`;
