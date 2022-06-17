456fc8475c9442a21476db0df4f65e8ddff92df4
Jean-Stefan <jeanss12@gmail.com>
2022-06-16 22:07:48 -0300

    Style: Changed Container to div

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
+<<<<<<< Updated upstream
 export const Container = styled.main`
+=======
+export const Container = styled.div`
+>>>>>>> Stashed changes
     display: flex;
     background: #ffffff;
 `;
